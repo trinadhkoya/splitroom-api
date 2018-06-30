19fd2951a3b29563e656ea0adc4e25f67d926319
Trinadh Koya <trinadh.koya@zaggle.in>
2018-07-01 01:07:40 +0530

    listing out rooms done

--- src/main/java/com/splitroom/splitroomapi/resource/RoomController.java
+++ src/main/java/com/splitroom/splitroomapi/resource/RoomController.java
@@ -1,9 +1,14 @@
 package com.splitroom.splitroomapi.resource;
 
+import com.splitroom.splitroomapi.model.RoomMember;
 import com.splitroom.splitroomapi.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
@@ -18,5 +23,52 @@ public class RoomController {
         return "Hi";
     }
 
+
+    @GetMapping("/rooms")
+    public List<RoomMember> getAllrooms() {
+        return roomRepository.findAll();
+    }
+
+
+    @GetMapping("/rooms/{id}")
+    public ResponseEntity<RoomMember> getRoomById(@PathVariable Long id) {
+        RoomMember roomMember = roomRepository.findOne(id);
+        if (roomMember == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(roomMember);
+    }
+
+
+    @PostMapping(value = "/rooms", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public RoomMember addRoom(@Valid @RequestBody RoomMember roomMember) {
+        return roomRepository.save(roomMember);
+    }
+
+
+    @PutMapping("/rooms/{id}")
+    public ResponseEntity<RoomMember> updateRoom(@PathVariable Long id,
+                                                 @Valid @RequestBody RoomMember roomDetails) {
+        RoomMember roomMember = roomRepository.findOne(id);
+        if (roomMember == null) {
+            return ResponseEntity.notFound().build();
+        }
+        roomMember.setRoomId(roomMember.getRoomId());
+        roomMember.setName(roomMember.getName());
+        RoomMember updatedRoom = roomRepository.save(roomMember);
+        return ResponseEntity.ok(updatedRoom);
+    }
+
+
+    @DeleteMapping("/rooms/{id}")
+    public ResponseEntity<RoomMember> deleteRoom(@PathVariable Long id) {
+        RoomMember roomMember = roomRepository.findOne(id);
+        if (roomMember == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        roomRepository.delete(roomMember);
+        return ResponseEntity.ok().build();
+    }
 
 }
